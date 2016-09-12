02f0abff4ef042af593b6b023478d45568403267
lesina <aleksey.lesovoy@phystech.edu>
2016-09-12 07:22:47 +0300

    Added text with solution

--- Laba02/exercise02.py
+++ Laba02/exercise02.py
@@ -6,6 +6,7 @@ y = x**2 - x - 6
 
 plt.subplot(121)
 plt.plot(x, y)
+plt.text(-2.75, 2, "x = - 2", fontsize = 20, color = 'green')
 plt.subplot(121).spines['bottom'].set_position('center')
 plt.subplot(121).spines['left'].set_position('center')
 plt.grid(True)
@@ -15,6 +16,7 @@ y = x**2 - x - 6
 
 plt.subplot(122)
 plt.plot(x, y)
+plt.text(2.2, 2, "x = 3", fontsize = 20, color = 'green')
 plt.subplot(122).spines['bottom'].set_position('center')
 plt.subplot(122).spines['left'].set_position('center')
 plt.grid(True)
