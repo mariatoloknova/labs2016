12d00dc2db6b45cae073bfab2f089dd0bd5a35db
lesina <lesha_lesovoy@mail.ru>
2016-09-29 19:56:30 +0300

    Nothing changed

--- Laba03/exercise01.py
+++ Laba03/exercise01.py
@@ -1,6 +1,6 @@
 n = int(input())
 for i in range(1, n):
     if (i % 2 == 0):
-        print(i, "even", sep = " ")
+        print(i , "even", sep = " ")
     else:
         print(i, "odd", sep = " ")
